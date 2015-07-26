eb6fc5d051970e35187fcf2dee1abc17afb091b1
krishna <krishna1010@hotmail.com>
2015-07-25 20:38:50 -0400

    updated assessment 3 R file

--- assessment3.R
+++ assessment3.R
@@ -23,33 +23,33 @@
    cache <- NULL 
 				                  
  # store a matrix 
-								                setMatrix <- function(newValue) { 
-             x <<- newValue 
+  setMatrix <- function(newValue) { 
+       x <<- newValue 
 # since the matrix is assigned a new value, flush the cache 
-             cache <<- NULL 
-									                    } 
-																       
+       cache <<- NULL 
+                   } 
+
 
  # returns the stored matrix 
   getMatrix <- function() { 
       x 
             } 
-																									    
+
 
  # cache the given argument  
      cacheInverse <- function(solve) { 
         cache <<- solve 
 									              } 
-																																         
+
 
  # get the cached value 
    getInverse <- function() { 
           cache 
            } 
-																																									               
+										               
  # return a list. Each named element of the list is a function 
    list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse) 
-										        } 
+        } 
 
 
 
